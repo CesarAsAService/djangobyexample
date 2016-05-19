ea90a5309d994babb6d21b2c6b38bba579491be8
Nilton Cesar <niltoncms@gmail.com>
2016-05-18 23:54:42 -0300

    Customizing extended ModelAdmin for showing Post model.

--- blog/core/admin.py
+++ blog/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'publish')
+	list_filter = ('status', 'created', 'author')
+	prepopulated_fields = { 'slug': ('title',) }
+	date_hierarchy = 'publish'
+
+admin.site.register(Post, PostAdmin)
