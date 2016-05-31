fe4e9d9a1cdbc766d610e348d4ffa70a6adaa057
Nilton Cesar <niltoncms@gmail.com>
2016-05-31 18:17:16 -0300

    Add first template that can show info retrieved from the database.

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def posts_list(request):
+	posts = Post.published.all()
+	template = 'posts.html'
+
+	return render(request, template, {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+	post = get_object_or_404(Post, slug=post, status='publicado', publish__year=year, publish__month=month, publish__day=day)
+	template = 'post_detail.html'
+
+	return render(request, template, {'post': post})
